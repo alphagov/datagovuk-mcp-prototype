af4f09cf56de5a682f3c1de69543574d5717d7bb
Obsiye <abdirizak.obsiye@dsit.gov.uk>
2026-07-02 14:24:01 +0100

    Ignore file extension .md

--- collections-mcp/server.py
+++ collections-mcp/server.py
@@ -15,7 +15,7 @@ async def get_content_for_collection_topic(collection_name: str, topic_name: str
         if collection != collection_name:
             continue
         for filename in os.listdir(base_dir.joinpath(collection)):
-            if filename != topic_name:
+            if filename != f"{topic_name}.md":
                 continue
             with open(base_dir.joinpath(collection).joinpath(filename), "r") as file:
                 result.append({
@@ -33,6 +33,7 @@ async def list_collections(collection_name: str | None = None) -> str:
     for collection in [f for f in os.listdir(base_dir) if os.path.isdir(base_dir.joinpath(f)) and f != "data"]:
         if collection_name and collection != collection_name:
             continue
+        collection = collection.replace(".md", "")
         result.append(collection)
     return json.dumps(result)
 
@@ -44,6 +45,7 @@ async def list_topics_in_a_collection(collection_name: str) -> str:
         if collection != collection_name:
             continue
         for filename in os.listdir(base_dir.joinpath(collection)):
+            filename = filename.replace(".md", "")
             result.append(filename)
     return json.dumps(result)
 
@@ -52,13 +54,14 @@ async def list_data_manual() -> str:
     result = []
     base_dir = pathlib.Path().resolve().joinpath("content", "data-manual")
     for filename in os.listdir(base_dir):
+        filename = filename.replace(".md", "")
         result.append(filename)
     return json.dumps(result)
 
 @mcp.tool()
 async def get_data_manual(filename: str) -> str:
     base_dir = pathlib.Path().resolve().joinpath("content", "data-manual")
-    with open(base_dir.joinpath(filename), "r") as file:
+    with open(base_dir.joinpath(f"{filename}.md"), "r") as file:
         return file.read()
 
 @mcp.tool()
